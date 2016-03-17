9fd8c93941222cf73df8bd1741a89eaf5991f53b
Philip Kamenarsky <p.kamenarsky@gmail.com>
2016-03-17 18:09:34 +0100

    Validate addtional fields

--- src/Thermite/Login.purs
+++ src/Thermite/Login.purs
@@ -142,9 +142,11 @@ render dispatch props state _
         , textinput' true (hoistValidator regState validateRepeatPassword) (Just Register) props.locale.repeatPassword (regState <<< regRepeatPassword)
         , [ button
               ( allValid state
-                [ hoistValidator regState validateEmail
-                , hoistValidator regState validateRepeatPassword
-                ]
+                $ [ hoistValidator regState validateEmail
+                  , hoistValidator regState validateRepeatPassword
+                  ]
+                  ++ map (\f -> hoistValidator (regState <<< regUserData) f.validate)
+                         props.additionalFields
               )
               state.regState.regLoading
               props.locale.register
@@ -215,7 +217,11 @@ withSessionId redirect sessionId@(RPC.SessionId sid) = do
       else return \st -> st { sessionId = Just sessionId }
 
 sendSync :: forall uid userdata err field b eff. (FromJSON b, ToJSON userdata, ToJSON uid, FromJSON uid, FromJSON err, ToJSON err) => (Config uid userdata err field (Effects eff)) -> (R.Proxy b -> UserCommand uid userdata err) -> Aff (Effects eff) (Either Error b)
-sendSync cfg req = ((lmap error <$> parseJSON) =<<) <$> cfg.sendRequest (req R.Proxy)
+-- sendSync cfg req = ((lmap (error <<< spy) <$> parseJSON) =<<) <$> cfg.sendRequest (req R.Proxy)
+sendSync cfg req = do
+  r <- cfg.sendRequest (req R.Proxy)
+  let a = (lmap error <$> parseJSON) =<< spy r
+  return $ spy a
 
 performAction :: forall uid userdata err field eff. (ToJSON userdata, ToJSON uid, FromJSON uid, FromJSON err, ToJSON err)
               => T.PerformAction (Effects eff) (State uid userdata err) (Config uid userdata err field (Effects eff)) (Action uid userdata err)
